4e1b1ad7ef6d82641dab7889afb58ef434924470
Gerardo Ponce <gerardo_apg_97@hotmail.com>
2019-12-04 04:49:28 -0600

    se agregaron vistas

--- public/ajax_handlers/signuoadmin.js
+++ public/ajax_handlers/signuoadmin.js
@@ -1,5 +1,4 @@
-$('#signup_form').submit(function(e){
-  e.preventDefault()
+$('#signup_button').on('click', function(){
   // cargar los valores de password, email, name, age
   let email = $("#email").val()
   let password = $("#password").val()
@@ -16,11 +15,9 @@ $('#signup_form').submit(function(e){
   json_to_send = JSON.stringify(json_to_send);
 
   //console.log(json_to_send)
-  
+
   $.ajax({
-    url: 'https://webproyectomacarena.herokuapp.com/users',
-    // url: 'http://localhost:3000/users',
-    // url: 'https://tuapp.herokuapp.com/users',
+    url: 'http://localhost:3000/users',
     headers: {
         'Content-Type':'application/json'
     },
@@ -30,10 +27,11 @@ $('#signup_form').submit(function(e){
     success: function(data){
       alert("Usuario creado con exito")
       //console.log('success: '+ data);
-      $('#id02').hide()
+      window.location = './login.html'
     },
     error: function(error_msg) {
       alert((error_msg['responseText']))
     }
   });
+
 });
